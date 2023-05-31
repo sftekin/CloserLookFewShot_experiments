02a089d352ab8e3dddcbd63ec8f7e79f31bb9571
sftekin <sftekin25@gmail.com>
2023-05-31 17:03:07 -0400

    ss backbones

---
+++ methods/ss_backbones/__init__.py
@@ -0,0 +1,6 @@
+from .ResNet import *
+from .DenseNet import *
+from .Conv4 import Conv4 as conv4
+from .Conv4 import Conv6 as conv6
+from .MobileNet import MobileNet as mobilenet
+from .WideResNet import wideres
